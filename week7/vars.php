37e778f9d0af04cf0fa98852e84e224891ac9644
Sophie <s3969368@student.rmit.edu.au>
2024-08-28 12:10:28 +1000

    deleted comments

---
+++ week7/vars.php
@@ -0,0 +1,8 @@
+<?php 
+$name = "Sophie";
+$age = 86;
+$number = 99.99;
+$wantsteaandcookies = true;
+
+echo "Hello, my name is $name, I look forward to being $age one day, and it is $number percent $wantsteaandcookies that I want tea and cookies";
+?>
