787ab372bfe24462546fde06fe8f5eb3e0294118
Sophie <s3969368@student.rmit.edu.au>
2024-10-30 15:30:57 +1100

    username in footer when logged in, alert displaying succesful log in

--- a3/index.php
+++ a3/index.php
@@ -2,7 +2,14 @@
 include('includes/header.inc');
 include('includes/nav.inc');
 include("includes/db_connect.inc");
-?>
+
+if (isset($_SESSION['usrmsg'])): ?>
+    <div class="alert alert-success alert-dismissible fade show" role="alert">
+        <?= htmlspecialchars($_SESSION['usrmsg']) ?>
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    </div>
+    <?php unset($_SESSION['usrmsg']); // Clear message after displaying ?>
+<?php endif; ?>
 
 <main class="main1">
         <div class="container">
